7e6c123209bcab015cc3402e83b62cc66c8edfd8
roberjank <roberjank@gmail.com>
2022-06-02 20:41:50 +0300

    rout, controller/MongoDB run

---
+++ tracker/server/routes/route.js
@@ -0,0 +1,8 @@
+//const routes = require("express");
+
+const routes = require("express").Router();
+const controller = require("../controller/controller");
+
+routes.route("/api/categories").get(controller.create_Categories);
+
+module.exports = routes;
--- tracker/server/server.js
+++ tracker/server/server.js
@@ -2,11 +2,33 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const port = 5000;
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
 
+//middleware
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Sever is running on port: ${port}`);
-});
+//mongoodb
+const conn = require("./db/connection.js");
+
+//routes
+app.use(require("./routes/route"));
+
+conn
+  .then((db) => {
+    if (!db) return process.exit(1);
+
+    // listen to the http server
+    app.listen(port, () => {
+      console.log(`Server is running on port: http://localhost:${port}`);
+    });
+
+    app.on("error", (err) =>
+      console.log(`Failed To Connect with HTTP Server : ${err}`)
+    );
+    // error in mondb connection
+  })
+  .catch((error) => {
+    console.log(`Connection Failed...! ${error}`);
+  });
