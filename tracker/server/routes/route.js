0b3938e9b67eeeecf92eadd0ea423e1299c86154
roberjank <roberjank@gmail.com>
2022-06-02 23:53:46 +0300

    categories+transaction

--- tracker/server/routes/route.js
+++ tracker/server/routes/route.js
@@ -1,5 +1,3 @@
-//const routes = require("express");
-
 const routes = require("express").Router();
 const controller = require("../controller/controller");
 
@@ -13,5 +11,7 @@ routes
   .post(controller.create_Transaction)
   .get(controller.get_Transaction)
   .delete(controller.delete_Transaction);
+
+routes.route("/api/labels").get(controller.get_Labels);
 
 module.exports = routes;
